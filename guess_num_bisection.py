db915fc5bfe3415d98ac29aa0f9987fb5816bbdb
Myron R Byrnes <34012809+ByrnesByrnes@users.noreply.github.com>
2018-01-10 15:49:15 -0500

    Add files via upload

---
+++ guess_num_bisection.py
@@ -0,0 +1,29 @@
+print("Please think of a number between 0 and 100!")
+
+user_num	= int(input("input a guess: "))
+
+num_guesses = 0
+low 	= 0
+high 	= 100
+guess = (high + low) / 2
+
+while user_num != guess:
+	print("is this your number {}?".format(guess))
+	""" User guess is either higher or lower. If user guess is L you want it to go up so
+	you respond with a high guess and vice versa."""
+	user_1 = input("high or low: h or l or is answer correct c? ")
+	if user_1 == "h":
+		high = guess
+
+	elif user_1 == "l":
+			low = guess
+				
+	elif user_1 == "c":
+		break
+	else:
+		print("input accuracte number")
+	guess = (high + low)/2
+	num_guesses += 1
+
+print("Game over. Your secret number was: {}".format(guess))
+
